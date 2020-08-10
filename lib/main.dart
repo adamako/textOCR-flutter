25979dc731ceb8fc7c5e4cfd211019f783ff4d4e
Adama Ko <adamako34@hotmail.com>
2020-08-10 11:30:42 +0000

    fix bugs

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TextOCR(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TextOCR());
   }
 }
